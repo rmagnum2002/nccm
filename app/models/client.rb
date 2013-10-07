a9b32ce857101ae3e712ed80dae8acf84b2b057c
Rosca Sergiu <rmagnum2002@gmail.com>
2013-10-07 10:25:16 +0300

    added grayscale variant for client avatar

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,11 @@ class Client < ActiveRecord::Base
   has_attached_file :avatar,
   :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
   :url => "/system/:attachment/:id/:style/:filename",
-  :styles => {:thumb => "144x144#" }
+  :styles => {:thumb => "144x144#", :grayscale => "144x144#"}
+
+  after_save :convert_grayscale
+
+  def convert_grayscale
+    system "convert public/system/avatars/#{self.id}/thumb/#{self.avatar.original_filename} -fx '(r+g+b)/3' public/system/avatars/#{self.id}/grayscale/#{self.avatar.original_filename}"
+  end
 end
